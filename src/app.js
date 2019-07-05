ef4e1086b9ff6d5071e5b25ac902db9184b92146
Paula Thomas <paula9t9@gmail.com>
2019-07-05 13:24:09 -0700

    Displays basic tasks

--- src/app.js
+++ src/app.js
@@ -1,25 +1,75 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './app.scss';
 
-function App() {
+import mockData from './mockData.json';
+
+const API = 'http://paula9t9-taskmaster.us-east-2.elasticbeanstalk.com/tasks';
+
+
+function Tasks() {
+
+  const [tasks, setTasks] = useState([]);
+
+  const getTasks = () => {
+    //Use mock data for dev purposes (comment out to switch to API)
+    // setTasks(mockData);
+
+    //Use API data for live deployment (comment in to switch to API)
+    fetch( API, {
+      mode: 'cors',
+    })
+    .then(data => data.json())
+    .then(tasks => setTasks(tasks))
+    .catch(console.err);
+  };
+
+  useEffect(getTasks, []);
+
+  function MoreInfo(props) {
+    let task = props.task || [];
+    return (
+      <section>
+          <div>
+            <span>{task.description}</span>
+            <span>Assigned to: {task.assignee}</span>
+          </div>
+      </section>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <ul>
+        {tasks.map( (task) => 
+          <li className={'status-${task.status}'} key={task.id}>
+            <details>
+              <summary>
+                <span>{task.title}</span>
+                <span>{task.status}</span>
+              </summary>
+              <MoreInfo task={task}></MoreInfo>
+            </details>
+          </li>
+          )}
+      </ul>
+  )
+}
+
+
+
+
+
+function App() {
+
+  return (
+    <>
+      <header>Taskmaster</header>
+      <main>
+        <Tasks></Tasks>
+      </main>
+      <footer>&copy;2019 PaulaT</footer>
+    </>
   );
 }
 
